297c92234569f3a0843209da574024ad449b4174
formcore <ironic.destruction@gmail.com>
2016-02-21 22:45:30 +0000

    Added files via upload
    Adding function to create database if it does not exist.
    re-adding the stars section, which was removed because I hadn't included the database that originally, was made manually in sqlite.

--- silty_ui.py
+++ silty_ui.py
@@ -7,8 +7,48 @@ import psutil
 import sqlite3
 from tkinter import *
 from PIL import ImageTk, Image
-def current_dir():
+def current_path():
     return os.path.dirname(os.path.realpath(sys.argv[0]))
+	
+def initialise_db():
+	db_path = current_path()+'/main.db'
+	connect = sqlite3.connect(db_path)
+	cursor = connect.cursor()
+	sql_script = open(current_path()+'/schema.sql', 'r')
+	cursor.executescript(sql_script.read())
+
+def get_stars():
+	db_path = current_path()+'/main.db'
+	connect = sqlite3.connect(db_path)
+	cursor = connect.cursor()
+	cursor.execute('select count(*) from stars')
+	total = cursor.fetchone()
+	cursor.execute('select cost from rewards where not award_date = ""')
+	rewards = cursor.fetchall()
+	spent = sum(sum(tuple) for tuple in rewards)
+	left = total[0] - spent
+	if connect:
+		connect.close()
+	return str(left) +' / '+ str(total[0])
+
+# toggle between system / processes window
+def left_panel_system(event):
+	global system
+	global processes
+	system_tab.config(state=ACTIVE)
+	processes_tab.config(state=NORMAL)
+	processes.grid_forget()
+	system.grid(row=1, sticky=E+W+N+S)
+	
+def left_panel_processes(event):
+	global system
+	global processes
+	processes_tab.config(state=ACTIVE)
+	system_tab.config(state=NORMAL)
+	system.grid_forget()
+	processes.grid(row=1, sticky=E+W+N+S)
+
+#get data for updates
 def status(program):
 	if program == 'nginx':
 		path = '/var/run/nginx.pid'
@@ -27,64 +67,21 @@ def uptime():
 		uptime_seconds = float(f.readline().split()[0])
 		uptime_string = str(timedelta(seconds = uptime_seconds))[:-10]
 	return uptime_string
+
 def cpu_usage():
 	return psutil.cpu_percent()
-def get_stars():
-	database = '/mnt/share/public/html/lib/database.db'
-	connect = sqlite3.connect(database)
-	cursor = connect.cursor()
-	cursor.execute('select count(*) from star_chart')
-	total = cursor.fetchone()
-	cursor.execute('select cost from rewards where not awarded_date = ""')
-	rewards = cursor.fetchall()
-	spent = sum(sum(tuple) for tuple in rewards)
-	left = total[0] - spent
-	if connect:
-		connect.close()
-	return str(left) +' / '+ str(total[0])
-
-def left_panel_system(event):
-	global system
-	global processes
-	system_tab.config(state=ACTIVE)
-	processes_tab.config(state=NORMAL)
-	processes.grid_forget()
-	system.grid(row=1, sticky=E+W+N+S)
 	
-def left_panel_processes(event):
-	global system
-	global processes
-	processes_tab.config(state=ACTIVE)
-	system_tab.config(state=NORMAL)
-	system.grid_forget()
-	processes.grid(row=1, sticky=E+W+N+S)
-	
-def update():
+# update sections with current data.
+def clock_update():
 	global clock
+	currtime.config(text=time.strftime('%I:%M'))
+	currdate.config(text=time.strftime('%d/%m/%Y'))
 	
-	global cpu
-	global ram
-	global pi_uptime
-	global share_used
-	global share_left
-
+def processes_update():
 	global nginx
 	global samba
 	global deluge
 	global mumble
-	
-	disk_dir = '/'
-	
-	currtime.config(text=time.strftime('%I:%M'))
-	currdate.config(text=time.strftime('%d/%m/%Y'))
-	cpu.config(text='cpu usage: '+str(cpu_usage())+'%')
-	pi_uptime.config(text='uptime: '+uptime())
-	vmem=psutil.virtual_memory()
-	ram.config(text='ram usage: '+str(vmem.percent)+'%')
-	share_usage=str(psutil.disk_usage(disk_dir).used>>30)[::1]+'gb'
-	share_left=str(psutil.disk_usage(disk_dir).free>>30)[::1]+'gb'
-	share_used.config(text='disk usage: '+share_usage)
-	share_free.config(text='disk free: '+share_left)
 	if status('nginx') == True:
 		nginx.config(text='webserver', bg='#88A8A7', fg='#F4EDE3')
 	else:
@@ -101,11 +98,38 @@ def update():
 		mumble.config(text='voip', bg='#88A8A7', fg='#F4EDE3')
 	else:
 		mumble.config(text='voip', bg='#e6005b')
+
+def system_update():
+	global cpu
+	global ram
+	global pi_uptime
+	global share_used
+	global share_left
 	
-	#star_chart.config(text=get_stars())
+	cpu.config(text='cpu usage: '+str(cpu_usage())+'%')
+	pi_uptime.config(text='uptime: '+uptime())
+	vmem=psutil.virtual_memory()
+	ram.config(text='ram usage: '+str(vmem.percent)+'%')
+	share_usage=str(psutil.disk_usage('/mnt/share').used>>30)[::1]+'gb'
+	share_left=str(psutil.disk_usage('/mnt/share/').free>>30)[::1]+'gb'
+	share_used.config(text='disk usage: '+share_usage)
+	share_free.config(text='disk free: '+share_left)
+
+def stars_update():
+	star_chart.config(text=get_stars())
+
+def update():	
+	clock_update()
+	processes_update()
+	system_update()
+	stars_update()
 	
 	root.after(1000, update)
-
+#test database existence, if not create it..
+if not os.path.isfile(current_path()+'/main.db'):
+	print("no db")
+	initialise_db()
+	
 root = Tk()
 root.title("silty_ui")
 # make it cover the entire screen
@@ -114,10 +138,10 @@ root.overrideredirect(1)
 root.geometry("%dx%d+0+0" % (w, h))
 root.focus_set() # <-- move focus to this widget
 root.bind("<Escape>", lambda e: e.widget.quit())
-#root.config(cursor="none")
+root.config(cursor="none")
 
-#create widgets
-title = Label(root, text="silty", font=('FreeSans', 14), pady=5, bg='#BC3522', fg='#F4EDE3')
+#create widgets w/ various styling attributes
+title = Label(root, text="♥ silty ♥", font=('FreeSans', 14), pady=5, bg='#BC3522', fg='#F4EDE3')
 clock = Frame(root,pady=20,bg="#F5B43E")
 currtime = Label(clock, font=('FreeSans', 40), bg="#F5B43E", fg='#F4EDE3')
 currdate = Label(clock, font=('FreeSans', 14), bg="#F5B43E", fg='#F4EDE3')
@@ -142,9 +166,7 @@ share_used= Label(system, padx=12, font=('FreeSans', 14),anchor=W, bg='#88A8A7',
 share_free= Label(system, padx=12, font=('FreeSans', 14),anchor=W, bg='#88A8A7', fg="#F4EDE3")
 
 stars = Frame(root, bg='#BC3522')
-
-image_dir = current_dir()
-
+image_dir = current_path()+'/img'
 curly_star=Image.open(image_dir+"/curlystar.png")
 curly_star=curly_star.resize((150,150), Image.ANTIALIAS)
 silty_star_image = ImageTk.PhotoImage(curly_star)
@@ -152,7 +174,7 @@ silty_star_image = ImageTk.PhotoImage(curly_star)
 silty_star = Label(stars, bg='#BC3522', fg='#F4EDE3', image = silty_star_image)
 star_chart = Label(stars, font=('FreeSans', 22), bg='#BC3522', fg='#F4EDE3')
 
-img = Image.open(image_dir+'/img1.png')
+img = Image.open(image_dir+"/curly.png")
 img = img.resize((165,220), Image.ANTIALIAS)
 
 pic = ImageTk.PhotoImage(img)
@@ -194,7 +216,8 @@ star_chart.grid(column=0, row=1, sticky=E+W+N, padx=0, pady=0, ipadx=0, ipady=0)
 panel.grid(row=2, column=2, sticky=N+E+W+S, ipadx=0, ipady=0, padx=0, pady=0)
 
 #Weighting
-Grid.rowconfigure(root, 0, weight=0)
+
+
 Grid.rowconfigure(root, 1, weight=1)
 Grid.rowconfigure(root, 2, weight=1)
 
@@ -203,12 +226,10 @@ Grid.columnconfigure(root, 1, weight=2)
 Grid.columnconfigure(root, 2, weight=1)
 
 Grid.columnconfigure(left_panel, 0, weight=1)
-Grid.rowconfigure(left_panel, 0, weight=0)
 Grid.rowconfigure(left_panel, 1, weight=1)
 
 Grid.columnconfigure(left_panel_tabs, 0, weight=1)
 Grid.columnconfigure(left_panel_tabs, 1, weight=1)
-Grid.rowconfigure(left_panel_tabs, 0, weight=0)
 
 Grid.columnconfigure(processes, 0, weight=1)
 Grid.rowconfigure(processes, 0, weight=1)
