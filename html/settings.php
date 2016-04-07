a7c9b4e62915fc5efc4f666ac050771644a42cf8
formcore <ironic.destruction@gmail.com>
2016-04-07 15:11:33 +0100

    Starts working on avatar support, I'll get back to it when I can be bothered.

--- html/settings.php
+++ html/settings.php
@@ -3,6 +3,7 @@ require "lib/password.php";
 include("lib/layout.php");
 include("lib/ironserver.php");
 authentication();
+$table="settings";
 ?>
 <?php
 //POST FORM FOR CHANGE USERNAME
@@ -17,6 +18,8 @@ if(isset($_POST["new_username"])){
             $prepare->bindParam(':new_username', $_POST["new_username"]);
             $prepare->bindParam(':old_username', $_SESSION["username"]);
             $result=$prepare->execute();
+        }else{
+            echo "<script type='text/javascript'>alert('username not changed')</script>";
         }
     }
     $dbh->close();
@@ -41,7 +44,9 @@ if(isset($_POST["current_password"])){
                 echo $dbh->lastErrorMsg();
                 exit();
             }
-		}
+		}else{
+            echo "<script type='text/javascript'>alert('password not changed')</script>";
+        }
 	}
 	$dbh->close();
 }
@@ -102,6 +107,60 @@ if(isset($_POST["resetuser"])&&isset($_POST["confirmresetuser"])&&$_POST["resetu
     $dbh->close();
 }
 
+//form for adding an avatar image, and I really don't want to mess around with files again ffs.
+/**if(isset($_FILES["image_upload"])){
+    $target_dir = $_SERVER['DOCUMENT_ROOT']."/img/avatars/";
+    $target_file = $target_dir . basename($_FILES["image_upload"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+    // Check if image file is a actual image or fake image
+    $check = getimagesize($_FILES["image_upload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+    // Check if file already exists
+    if (file_exists($target_file)) {
+        echo "Sorry, file already exists.";
+        $uploadOk = 0;
+    }
+    // Check file size
+    if ($_FILES["fileToUpload"]["size"] > 500000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+    }
+    // Allow certain file formats
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif" ) {
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOk = 0;
+    }
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+    // if everything is ok, try to upload file
+    } else {
+        if (move_uploaded_file($_FILES["image_upload"]["tmp_name"], $target_file)) {
+            echo "The file ". $target_file. " has been uploaded.";
+            $dbh = new sqlite3('../main.db');
+            $prepare = $dbh->prepare("UPDATE users SET avatar=? WHERE username=?");
+            $image_path=$target_file;
+            $prepare->bindParam(1, $image_path);
+            $prepare->bindParam(2, $_SESSION["username"]);
+            $result = $prepare->execute();
+            if(!$result){
+                echo $dbh->lastErrorMsg();
+            }
+            $dbh->close();
+            #header("location: ../settings.php");
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
+}**/
 ?>
 <html>
 <?php
@@ -138,15 +197,18 @@ echo "<h1>Welcome " . $_SESSION["username"] . "</h1>";
 <form name="change_password" action="settings.php" method="post">
 <label for="password">Current Password:</label>
 <input type="password" name="current_password">
-<br>
 <label for="new_password">New Password:</label>
 <input type="password" name="new_password">
-<br>
 <label for="confirm_password">Confirm Password:</label>
 <input type="password" name="confirm_password">
-<br>
 <input type="submit" value="submit">
 </form>
+<!--<h2>Set avatar</h2>
+<form  name="image_upload"action="settings.php" method="post" enctype="multipart/form-data">
+<label for="image_upload">Upload avatar:</label>
+<input type="file" name="image_upload" id="image_upload">
+<input type="submit" value="submit">
+</form>-->
 <?php
 if($_SESSION['user_id'] == 1){
 	echo '<h1>Admin controls</h1>';
